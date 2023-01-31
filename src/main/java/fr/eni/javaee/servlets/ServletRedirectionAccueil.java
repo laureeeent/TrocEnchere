6525adf0e6c93ed6d6fa3957ed35fd6288015d39
lchibois2022 <lchibois2022@1SE2-FTQJVN2.ad.campus-eni.fr>
2023-01-31 09:21:19 +0100

    ADD : javax.servlet lib essentiel au bon fonctionnement du projet. web.xml acceuille le projet sur un mapping et plus directement index.html

---
+++ src/main/java/fr/eni/javaee/servlets/ServletRedirectionAccueil.java
@@ -0,0 +1,46 @@
+package fr.eni.javaee.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletRedirectionAccueil
+ */
+@WebServlet("/ServletRedirectionAccueil")
+public class ServletRedirectionAccueil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rs = request.getRequestDispatcher("/index.html");
+		rs.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
