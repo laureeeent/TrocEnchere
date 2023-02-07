cfc42d01521c30e2d152ef3777894272ceacff1f
lchibois2022 <lchibois2022@1SE2-FTQJVN2.ad.campus-eni.fr>
2023-02-07 17:16:08 +0100

    Merged

--- src/main/java/fr/eni/javaee/servlets/AfficherArticle.java
+++ src/main/java/fr/eni/javaee/servlets/AfficherArticle.java
@@ -33,15 +33,10 @@ public class AfficherArticle extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 
-<<<<<<< HEAD
-		int numArt =  Integer.parseInt(request.getParameter("id"));
-		System.out.println("numart = "+numArt);
-=======
 		String id_article = request.getParameter("id");
 		int numArt =  Integer.parseInt(id_article);
 		System.out.println("num: "+ numArt);
 
->>>>>>> branch 'main' of https://github.com/laureeeent/TrocEnchere.git
 		HttpSession session = request.getSession();
 		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");;
 		if (user != null){
@@ -51,21 +46,13 @@ public class AfficherArticle extends HttpServlet {
 			ArticleVendu artById = null;
 			try {
 				artById = articleManager.selectionnerByID(numArt);
-<<<<<<< HEAD
-				System.out.println(artById.getEnchere().getAcheteur().getPseudo());
-=======
 				System.out.println("article :" +artById);
->>>>>>> branch 'main' of https://github.com/laureeeent/TrocEnchere.git
 			} catch (BusinessException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			request.setAttribute("articleById", artById);
-<<<<<<< HEAD
-			
-=======
 			System.out.println(artById);
->>>>>>> branch 'main' of https://github.com/laureeeent/TrocEnchere.git
 			rs.forward(request, response);
 		}else {
 			RequestDispatcher rs = request.getRequestDispatcher("ServletConnecter");
