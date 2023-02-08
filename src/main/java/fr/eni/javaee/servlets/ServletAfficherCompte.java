a94ac883a0ad16b23804ae862dc6b15c01e9ec8f
mboulon2022 <mboulon2022@1SE2-2WQJVN2.ad.campus-eni.fr>
2023-02-08 16:34:49 +0100

    change : voir profil d'un autre utilisateur

--- src/main/java/fr/eni/javaee/servlets/ServletAfficherCompte.java
+++ src/main/java/fr/eni/javaee/servlets/ServletAfficherCompte.java
@@ -10,12 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.eni.javaee.bll.UtilisateurManager;
 import fr.eni.javaee.bo.Utilisateur;
 //import javax.servlet.http.HttpSession;
 //
 //import fr.eni.javaee.bll.UtilisateurManager;
 //import fr.eni.javaee.bo.Utilisateur;
 //import fr.eni.javaee.exceptions.BusinessException;
+import fr.eni.javaee.exceptions.BusinessException;
 
 /**
  * Servlet implementation class AfficherCompte
@@ -28,12 +30,42 @@ public class ServletAfficherCompte extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
+		String pseudo = (String) request.getAttribute("param_pseudo");
+		RequestDispatcher rs = null;
+
 		
-//		UtilisateurManager utilsateurManager = new UtilisateurManager();
+		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
+		UtilisateurManager utilsateurManager = new UtilisateurManager();
+		Utilisateur userAffichage = null;
+		String param_pseudo = null;
+		if (pseudo != null) {
+	
+		try {
+			userAffichage = utilsateurManager.getUtilisateur(pseudo);
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-//		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
-//		Utilisateur userAffichage;
+		param_pseudo = userAffichage.getPseudo();
+		
+		if (user != null) {
+				if (user.getPseudo().equals(param_pseudo)) {
+					rs = request.getRequestDispatcher("./WEB-INF/JSP/pageAffichagePseudo.jsp");
+					rs.forward(request, response);
+					System.out.println("c'est le meme gars normalement");
+				}else {
+					request.setAttribute("autre_profil", userAffichage);
+					session.getAttribute("utilisateur");
+					System.out.println("c'est PAS le meme gars normalement");
+					rs = request.getRequestDispatcher("./WEB-INF/JSP/pageAffichagePseudo.jsp");
+					rs.forward(request, response);
+				}
+				}
+			}
+		
+
 //		try {
 //			userAffichage = utilsateurManager.getUtilisateur( (String)request.getAttribute("utilisateurAffichage_nom") );
 //			request.setAttribute("utilisateurAffichage", userAffichage);
@@ -44,15 +76,19 @@ public class ServletAfficherCompte extends HttpServlet {
 //			}
 //			e.printStackTrace();
 //		}
-		RequestDispatcher rs = null;
-		HttpSession session = request.getSession();
-		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
+		
+//		RequestDispatcher rs = null;
+//		HttpSession session = request.getSession();
+//		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
+//		String util = request.getParameter("pseudo");
+//		System.out.println(util);
 		
 		if (user == null) {
 			rs = request.getRequestDispatcher("/WEB-INF/JSP/erreurConnexionUtilisateur.jsp");
 			rs.forward(request, response);
 		}
-		
+		System.out.println("pas de pseudo donc meme gars afficher");
+		request.setAttribute("pseudo", pseudo);
 		rs = request.getRequestDispatcher("./WEB-INF/JSP/pageAffichagePseudo.jsp");
 		rs.forward(request, response);
 	}
--- src/main/webapp/WEB-INF/JSP/pageAffichagePseudo.jsp
+++ src/main/webapp/WEB-INF/JSP/pageAffichagePseudo.jsp
@@ -12,8 +12,9 @@
 <header id="h_principal">
 	<%@include file="fragment/div_id_entete.jsp"%>
 </header>
-			<c:if test="${utilisateur != null}">
+			<c:if test="${utilisateur != null && utilisateur.equals(param_pseudo) || pseudo == null}">
 				<div>
+
 					<p id="lignePseudo">Pseudo :       ${utilisateur.getPseudo() }</p>
 					<p id="ligneNom">Nom :       ${utilisateur.getNom() }</p>
 					<p id="lignePrenom">Prenom :       ${utilisateur.getPrenom() }</p>
@@ -22,13 +23,26 @@
 					<p id="ligneRue">Rue :       ${utilisateur.getRue() }</p>
 					<p id="ligneCodePostal">Code postal :       ${utilisateur.getCodePostal() }</p>
 					<p id="ligneVille">Ville :       ${utilisateur.getVille() }</p>
+					<a href="./ModificationCompte"><input type="button" value="Modifier mon compte"></a>
+				</div>
+			</c:if>
+						<c:if test="${utilisateur != null && utilisateur != param_pseudo && autre_profil != null}">
+				<div>
+
+					<p id="lignePseudo">Pseudo :       ${autre_profil.getPseudo() }</p>
+					<p id="ligneNom">Nom :       ${autre_profil.getNom() }</p>
+					<p id="lignePrenom">Prenom :       ${autre_profil.getPrenom() }</p>
+					<p id="ligneEmail">Email :       ${autre_profil.getEmail() }</p>
+					<p id="ligneTelephone">Téléphone :       ${autre_profil.getTelephone() }</p>
+					<p id="ligneRue">Rue :       ${autre_profil.getRue() }</p>
+					<p id="ligneCodePostal">Code postal :       ${autre_profil.getCodePostal() }</p>
+					<p id="ligneVille">Ville :       ${autre_profil.getVille() }</p>
+
 				</div>
 			</c:if>
 
-		<form action="ServletRedirectionAccueil" method="get">
-			<button type="submit" ><h3>Retour Acceuil</h3></button>
-		</form>
-		<a href="./ModificationCompte"><input type="button" value="Modifier mon compte"></a>
+<%@include file="fragment/bouttonRetourAccueil.jsp" %>
+
 
 </body>
 </html>
